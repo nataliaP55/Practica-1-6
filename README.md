8647ffc97882bfaed35b2a76575299746f6a371a
yoenotracompu <yoenotracompu@otracompu.com>
2022-04-19 15:16:19 -0300

    utilización de prompt y tipos de datos

---
+++ README.md
@@ -0,0 +1,2 @@
+clase 8 
+utilización de Prompt y tipos de datos (string, boolean, float, integer Z)
---
+++ algoritmo.js
@@ -0,0 +1,15 @@
+var respuesta = prompt ("Ingrese su nombre", ["Nombre"]); // escribe mediante el navegador
+
+console.log( respuesta ) // escribe mediante la consola
+
+document.write(`
+ <h4>Nombre: </4>    
+ <h4>${ respuesta }</4>  
+   
+`)
+
+//defino una variable (palabra, numero, decimal, booleano) y la inicializo con un valor literal de el tipo de dato que usé
+var string = "texto"; // tipo de dato String
+var numero = 2; // tipo de dato integer
+var decimal = 2.1; // tipo de dato float
+var booleano = false; // tipo de dato boolean        
